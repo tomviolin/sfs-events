62937e7c5e95d6d9c2b4ba812981e9034ee8710c
Tom Hansen <tomh@uwm.edu>
2022-10-18 12:55:20 -0500

    local roar for now

--- js/custom_display.js
+++ js/custom_display.js
@@ -1,7 +1,8 @@
 // Edit your ics sources here
 ics_sources = [
 	{url:'/proxy.php?url='+encodeURI('http://uwm.edu/freshwater/events/?ical=1'), title:'SFS Web Events', prefix:'💧', event_properties:{color: '#FFbbFF'}},
-	{url:'/proxy.php?url='+encodeURI('https://panthers.sharepoint.com/:u:/s/SFSReservations/EUOq58_EkORIrjNUmg7nZ8gBVOUY04mdOpL8iMZXzQzgxA?e=5m1Rd2'), title:'ROAR Room Reservations', prefix:'🚪', event_properties:{color: '#ccffcc'}},
+	//{url:'/proxy.php?url='+encodeURI('https://panthers.sharepoint.com/:u:/s/SFSReservations/EUOq58_EkORIrjNUmg7nZ8gBVOUY04mdOpL8iMZXzQzgxA?e=5m1Rd2'), title:'ROAR Room Reservations', prefix:'🚪', event_properties:{color: '#ccffcc'}},
+	{url:'/proxy.php?url='+encodeURI('http://waterbase.uwm.edu/roarcalendars/GLRF_ALL.ics'), title:'ROAR Room Reservations', prefix:'🚪', event_properties:{color: '#ccffcc'}},
 	{url:'/proxy.php?url='+encodeURI('https://calendar.google.com/calendar/ical/sfs.neeskay%40gmail.com/public/basic.ics'),title:'R/V Neeskay', prefix:'⛵', event_properties:{color:'#cceeFF'}},
 	{url: '/proxy.php?url='+encodeURI('https://calendar.google.com/calendar/ical/sfs.events.calendar%40gmail.com/public/basic.ics'), title:'SFS Internal Events', prefix:'🏢', event_properties:{color:'#ffddbb'}}
 ]
