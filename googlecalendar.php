b63f05a22837f9a6d9bb76fdc4a1289bbd02bee2
Tom Hansen <tomh@uwm.edu>
2023-04-03 22:01:59 -0500

    google calendar

--- googlecalendar.php
+++ googlecalendar.php
@@ -3,6 +3,7 @@
 //$src = $_GET['src'];
 //$ctz = $_GET['ctz'];
 header("Content-type: text/html");
+header("Content-Security-Policy: frame-ancestors 'self' https://www.uwm.edu;");
 echo '<base href="https://calendar.google.com/">'+"\n";
 echo file_get_contents("https://calendar.google.com/calendar/u/0/embed?src=e8e4d8c6474e67b7b5f83c80f7cf29acb183864ed61f634e4d106e661713c46a@group.calendar.google.com&ctz=America/Chicago");
 
